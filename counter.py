3b6edc2b7d5aa8d38bc65f3f747146145c518183
apurvaraman <apurva.raman@students.olin.edu>
2016-03-13 00:28:41 -0500

    Pickling toolbox implemented and tested!

--- counter.py
+++ counter.py
@@ -29,11 +29,24 @@ def update_counter(file_name, reset=False):
 	>>> update_counter('blah2.txt')
 	2
 	"""
-	pass
+	if not exists(file_name) or reset:
+		dump(1, open(file_name, "w"))  #open in writing mode if it does not exists or if resetting counter
+		return 1
+
+	else: 
+		reading = open(file_name, "r+")  #open in reading plus mode if it exists and if resetting counter
+		count = load(reading)
+		count += 1
+		reading.seek(0,0)
+		dump(count, reading)
+		return count
+
+	f.close()
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		import doctest
-		doctest.testmod()
+		doctest.testmod(verbose= True)
 	else:
 		print "new value is " + str(update_counter(sys.argv[1]))
+		
